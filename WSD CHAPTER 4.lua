3f79dd13d02263e32d1c981a2b3491afcb8bda48
Iwhpdqkwhrjsjhsuage-wie <asyidik77@gmail.com>
2024-09-01 15:51:09 +0700

    Add files via upload

--- WSD CHAPTER 4.lua
+++ WSD CHAPTER 4.lua
@@ -4,6 +4,7 @@ for _, gui in ipairs(game:GetService("CoreGui"):GetChildren()) do
     end
 end
 
+getgenv().BreakLoopAutoSeat = false
 getgenv().Active = false
 getgenv().BreakLoopAutoDrink = false
 getgenv().BreakLoopAutoEat = false
@@ -50,17 +51,43 @@ local function opengate()
   end
 end
 
+local function seat()
+  if game.Players.LocalPlayer.Character.Humanoid.Sit == true then return end
+  local old = rootpart.CFrame
+  rootpart.CFrame = CFrame.new(151.418716, 192.515686, 82.5042191, 1, 0, 0, 0, 0.998586237, 0.0531555787, 0, -0.0531555787, 0.998586237)
+  for i, v in pairs(workspace.Seats:GetDescendants()) do
+    if v.Name == "Primary" and v.Parent:IsA("Model") then
+      local jarak = (v.Position - game.Players.LocalPlayer.Character.HumanoidRootPart.Position).Magnitude
+      if jarak <= 5 then
+        fireproximityprompt(v:FindFirstChild("ProximityPrompt"))
+        if game.Players.LocalPlayer.Character.Humanoid.Sit == true then return end
+      end
+    end
+  end
+end
+
 local function eat()
   getgenv().Active = true
   local old = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+  if game.Players.LocalPlayer.Character.Humanoid.Sit == true then
+    game.Players.LocalPlayer.Character.Humanoid.Jump = true
+  end
   rootpart.CFrame = CFrame.new(160.213379, 192.0858, 116.197739, -0.181649685, 4.62826533e-09, -0.98336333, 2.58541855e-09, 1, 4.22898072e-09, 0.98336333, -1.77421289e-09, -0.181649685)
   task.wait(0.1)
   fireproximityprompt(workspace.Foodies.FridgeNoodles.Primary.ProximityPrompt)
   task.wait(0.2)
+  for i, v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
+    if v.Name == "Raw Noodle" then
+      game.Players.LocalPlayer.Character.Humanoid:EquipTool(v)
+      game.Players.LocalPlayer.Character.Humanoid:EquipTool(v)
+      game.Players.LocalPlayer.Character.Humanoid:EquipTool(v)
+    end
+  end
+  task.wait(0.3)
   fireproximityprompt(workspace.Foodies.Stove.Primary.ProximityPrompt)
   task.wait(0.2)
   game:GetService("VirtualUser"):ClickButton1(Vector2.new(9e9, 9e9))
-  task.wait(0.1)
+  task.wait(0.2)
   rootpart.CFrame = old
   getgenv().Active = false
 end
@@ -68,17 +95,36 @@ end
 local function drink()
   getgenv().Active = true
   local old = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+  if game.Players.LocalPlayer.Character.Humanoid.Sit == true then
+    game.Players.LocalPlayer.Character.Humanoid.Jump = true
+  end
   rootpart.CFrame = CFrame.new(160.213379, 192.0858, 116.197739, -0.181649685, 4.62826533e-09, -0.98336333, 2.58541855e-09, 1, 4.22898072e-09, 0.98336333, -1.77421289e-09, -0.181649685)
   if game.Players.LocalPlayer.Backpack:FindFirstChild("Drinking Glass") == nil or game.Players.LocalPlayer.Backpack:FindFirstChild("Water of Glass") == nil then
     fireproximityprompt(workspace.Foodies["Shelf with Drinks"].Primary.ProximityPrompt)
     task.wait(0.2)
+    for i, v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
+      if v.Name == "Drinking Glass" then
+        game.Players.LocalPlayer.Character.Humanoid:EquipTool(v)
+        game.Players.LocalPlayer.Character.Humanoid:EquipTool(v)
+        game.Players.LocalPlayer.Character.Humanoid:EquipTool(v)
+      end
+    end
+    task.wait(0.2)
     fireproximityprompt(workspace.Foodies.WaterDispenser.Primary.ProximityPrompt)
     task.wait(0.2)
     game:GetService("VirtualUser"):ClickButton1(Vector2.new(9e9, 9e9))
+    game:GetService("VirtualUser"):ClickButton1(Vector2.new(9e9, 9e9))
     task.wait(0.1)
     rootpart.CFrame = old
-  end
-  if game.Players.LocalPlayer.Backpack:FindFirstChild("Drinking Glass") ~= nil or game.Players.LocalPlayer.Backpack:FindFirstChild("Water of Glass") ~= nil then
+  elseif game.Players.LocalPlayer.Backpack:FindFirstChild("Drinking Glass") ~= nil or game.Players.LocalPlayer.Backpack:FindFirstChild("Water of Glass") ~= nil then
+    for i, v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
+      if v.Name == "Drinking Glass" then
+        game.Players.LocalPlayer.Character.Humanoid:EquipTool(v)
+        game.Players.LocalPlayer.Character.Humanoid:EquipTool(v)
+        game.Players.LocalPlayer.Character.Humanoid:EquipTool(v)
+      end
+    end
+    task.wait(0.2)
     fireproximityprompt(workspace.Foodies.WaterDispenser.Primary.ProximityPrompt)
     task.wait(0.2)
     game:GetService("VirtualUser"):ClickButton1(Vector2.new(9e9, 9e9))
@@ -91,11 +137,22 @@ end
 local function generator()
   getgenv().Active = true
   local old = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+  if game.Players.LocalPlayer.Character.Humanoid.Sit == true then
+    game.Players.LocalPlayer.Character.Humanoid.Jump = true
+  end
   rootpart.CFrame = CFrame.new(160.213379, 192.0858, 116.197739, -0.181649685, 4.62826533e-09, -0.98336333, 2.58541855e-09, 1, 4.22898072e-09, 0.98336333, -1.77421289e-09, -0.181649685)
   task.wait(0.2)
   for i, v in pairs(workspace:GetChildren()) do
     if v:FindFirstChild("Primary") and v.Name == "GasCan" and v:IsA("Model") then
       fireproximityprompt(v:FindFirstChild("Primary").ProximityPrompt)
+    end
+  end
+  task.wait(0.2)
+  for i, v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
+    if v.Name == "gas can" then
+      game.Players.LocalPlayer.Character.Humanoid:EquipTool(v)
+      game.Players.LocalPlayer.Character.Humanoid:EquipTool(v)
+      game.Players.LocalPlayer.Character.Humanoid:EquipTool(v)
     end
   end
   task.wait(0.2)
@@ -106,15 +163,22 @@ local function generator()
 end
 
 local function repairwindow()
+  getgenv().Active = true
+  local old = rootpart.CFrame
+  if game.Players.LocalPlayer.Character.Humanoid.Sit == true then
+    game.Players.LocalPlayer.Character.Humanoid.Jump = true
+  end
   for i, v in pairs(workspace.Windows:GetDescendants()) do
     if v:IsA("Part") or v:IsA("MeshPart") and v.Name == "Window" then
       if v:FindFirstChild("ProximityPrompt") ~= nil then
         rootpart.CFrame = v.CFrame
-        task.wait(0.2)
+        fireproximityprompt(v:FindFirstChild("ProximityPrompt"))
         fireproximityprompt(v:FindFirstChild("ProximityPrompt"))
       end
     end
   end
+  rootpart.CFrame = old
+  getgenv().Active = false
 end
 
 local function Esp()
@@ -135,6 +199,7 @@ local function Esp()
       TextLabel.TextSize = 20
       TextLabel.BackgroundColor3 = Color3.new(255, 255, 255)
       TextLabel.BackgroundTransparency = 1
+      TextLabel.TextStrokeTransparency = 0
       TextLabel.Position = UDim2.new(0, 0, 0, -20)
       TextLabel.Font = Enum.Font.SourceSans
       TextLabel.Text = "MOM"
@@ -188,10 +253,10 @@ end
 local Input = Tabs.Main:AddInput("WalkSpeed", {
     Title = "WalkSpeed",
     Description = "",
-    Default = "9",
+    Default = "5",
     Placeholder = "Type Here!",
     Numeric = true, -- Only allows numbers
-    Finished = false, -- Only calls callback when you press enter
+    Finished = true, -- Only calls callback when you press enter
     Callback = function(Value)
         game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Value
     end
@@ -216,6 +281,23 @@ Tabs.Main:AddButton({
     end
 })
 
+local Toggle = Tabs.Main:AddToggle("Instant Interact", 
+{
+    Title = "Instant Interact", 
+    Description = "",
+    Default = false,
+    Callback = function(state)
+      if state == true then
+        instant = game:GetService("ProximityPromptService").PromptButtonHoldBegan:Connect(function(prompt)
+          fireproximityprompt(prompt)
+        end)
+      end
+      if state == false then
+        instant:Disconnect()
+      end
+    end 
+})
+
 Tabs.Main:AddButton({
     Title = "Open Gate",
     Description = "",
@@ -225,14 +307,35 @@ Tabs.Main:AddButton({
           v.Anchored = false
         end
       end
+      if game.Players.LocalPlayer.Backpack:FindFirstChild("Hammer") ~= nil then
+        game.Players.LocalPlayer.Backpack:FindFirstChild("Hammer"):Destroy()
+      end
     end
+})
+
+local Toggle = Tabs.Main:AddToggle("Instant Interact", 
+{
+    Title = "Open Gate (Spamming Click)", 
+    Description = "",
+    Default = false,
+    Callback = function(state)
+      if state == true then
+        getgenv().BreakLoopGate = false
+        opengate()
+      end
+      if state == false then
+        getgenv().BreakLoopGate = true
+      end
+    end 
 })
 
 Tabs.Main:AddButton({
     Title = "Repair Broken Window",
     Description = "",
     Callback = function()
-      repairwindow()
+      if getgenv().Active == false and workspace.Game.PossesedMom.ChaseDoor.Value == false then
+        repairwindow()
+      end
     end
 })
 
@@ -254,39 +357,6 @@ local Dropdown = Tabs.Main:AddDropdown("Do", {
     end
 })
 
-local Toggle = Tabs.Main:AddToggle("Instant Interact", 
-{
-    Title = "Open Gate (Spamming Click)", 
-    Description = "",
-    Default = false,
-    Callback = function(state)
-      if state == true then
-        getgenv().BreakLoopGate = false
-        opengate()
-      end
-      if state == false then
-        getgenv().BreakLoopGate = true
-      end
-    end 
-})
-
-local Toggle = Tabs.Main:AddToggle("Instant Interact", 
-{
-    Title = "Instant Interact", 
-    Description = "",
-    Default = false,
-    Callback = function(state)
-      if state == true then
-        instant = game:GetService("ProximityPromptService").PromptButtonHoldBegan:Connect(function(prompt)
-          fireproximityprompt(prompt)
-        end)
-      end
-      if state == false then
-        instant:Disconnect()
-      end
-    end 
-})
-
 local Toggle = Tabs.Main:AddToggle("Auto Repair Broken Window", 
 {
     Title = "Auto Repair Broken Window", 
@@ -297,7 +367,9 @@ local Toggle = Tabs.Main:AddToggle("Auto Repair Broken Window",
         getgenv().BreakLoopWindow = false
         while wait() do
           task.wait(0.3)
-          repairwindow()
+          if getgenv().Active == false and workspace.Game.PossesedMom.ChaseDoor.Value == false then
+            repairwindow()
+          end
           if getgenv().BreakLoopWindow == true then
             break
           end
@@ -339,6 +411,7 @@ local Toggle = Tabs.Main:AddToggle("Auto Drink",
     Default = false,
     Callback = function(state)
         if state == true then
+          getgenv().BreakLoopAutoDrink = false
           while wait() do
             task.wait(0.4)
             if getgenv().Active == false and workspace.Game.PossesedMom.ChaseDoor.Value == false and game.Players.LocalPlayer.Thirst.Value <= 50 then
@@ -362,6 +435,7 @@ local Toggle = Tabs.Main:AddToggle("Auto Eat",
     Default = false,
     Callback = function(state)
         if state == true then
+          getgenv().BreakLoopAutoEat = false
           while wait() do
             task.wait(0.5)
             if getgenv().Active == false and workspace.Game.PossesedMom.ChaseDoor.Value == false and game.Players.LocalPlayer.Hunger.Value <= 50 then
@@ -385,6 +459,7 @@ local Toggle = Tabs.Main:AddToggle("Auto Fill Generator",
     Default = false,
     Callback = function(state)
         if state == true then
+          getgenv().BreakLoopAutoFillGenerator = false
           while wait() do
             task.wait(0.3)
             if getgenv().Active == false and workspace.Game.PossesedMom.ChaseDoor.Value == false and workspace.Generator.Bar.Value <= 10 then
@@ -400,6 +475,31 @@ local Toggle = Tabs.Main:AddToggle("Auto Fill Generator",
         end
     end 
 })
+
+local Toggle = Tabs.Main:AddToggle("Auto Seat", 
+{
+    Title = "Auto Seat (BETA)", 
+    Description = "",
+    Default = false,
+    Callback = function(state)
+        if state == true then
+          getgenv().BreakLoopAutoSeat = false
+          while wait() do
+            task.wait(0.2)
+            if getgenv().Active == false and workspace.Game.PossesedMom.ChaseDoor.Value == true and game.Players.LocalPlayer.Character.Humanoid.Sit == false then
+              seat()
+            end
+            if getgenv().BreakLoopAutoSeat == true then
+              break
+            end
+          end
+        end
+        if state == false then
+            getgenv().BreakLoopAutoSeat = true
+        end
+    end 
+})
+
 
 ----------------------- CONFIG TAB ----------------------
 
